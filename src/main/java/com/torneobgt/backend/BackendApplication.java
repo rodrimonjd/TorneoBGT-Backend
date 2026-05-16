0d4d7f8e502fe45df4327f271079edde09de5d5f
rodrimonjd <juandarodriguez2203@gmail.com>
2026-05-15 19:35:59 -0500

    cambios para conectar con mongodb

--- src/main/java/com/torneobgt/backend/BackendApplication.java
+++ src/main/java/com/torneobgt/backend/BackendApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BackendApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        var ctx = SpringApplication.run(BackendApplication.class, args);
+        var env = ctx.getEnvironment();
+        System.out.println(">>>>>> MONGO URI: " + env.getProperty("spring.data.mongodb.uri"));
+    }
 }
